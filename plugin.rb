baade615284cffa7bf001aa37522824bd5f8d382
VolkMicro <153424262+VolkMicro@users.noreply.github.com>
2024-12-28 15:31:42 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,15 +1,20 @@
 # name: discourse-private-topics
 # about: Allows to keep topics private to the topic creator and specific groups.
-# version: 1.5.7
-# authors: Communiteq
-# meta_topic_id: 268646
+# version: 1.5.8
+# authors: Communiteq, Optimized by AI
 # url: https://github.com/communiteq/discourse-private-topics
 
 enabled_site_setting :private_topics_enabled
 
 module ::DiscoursePrivateTopics
-  # Кэш для уже скрытых тем
-  @@hidden_topics_cache = Set.new
+  # Используем Redis для хранения скрытых тем
+  def self.hidden_topics_cache
+    @hidden_topics_cache ||= Set.new(Redis.current.smembers("hidden_topics").map(&:to_i))
+  end
+
+  def self.add_to_cache(topic_id)
+    Redis.current.sadd("hidden_topics", topic_id)
+  end
 
   # Функция для скрытия темы
   def self.hide_topic(topic)
@@ -23,31 +28,30 @@ module ::DiscoursePrivateTopics
     Rails.logger.info("Тема #{topic.id} успешно скрыта.")
     
     # Добавляем тему в кэш
-    @@hidden_topics_cache.add(topic.id)
+    add_to_cache(topic.id)
   end
 
-  # Функция для обработки всех тем в категории
+  # Функция для обработки всех тем
   def self.process_topics
-    Category.find_each do |category|
-      category.topics.find_each do |topic|
-        # Пропускаем тему с определенным ID
-        next if topic.id == EXCLUDED_TOPIC_ID 
-        
-        # Пропускаем, если тема уже скрыта
-        next if @@hidden_topics_cache.include?(topic.id)
+    Topic.where.not(pinned: true).where.not(hidden: true).find_each do |topic|
+      # Пропускаем тему с определенным ID из настроек
+      excluded_topic_id = SiteSetting.excluded_topic_id.to_i rescue -1
+      next if topic.id == excluded_topic_id
 
-        # Вызываем функцию для скрытия темы
-        hide_topic(topic)
-      end
+      # Пропускаем, если тема уже скрыта
+      next if hidden_topics_cache.include?(topic.id)
+
+      # Вызываем функцию для скрытия темы
+      hide_topic(topic)
     end
   end
 
   # Получает список пользователей, которым всегда показываются темы
   def self.get_unfiltered_user_ids(user)
-    user_ids = [ Discourse.system_user.id ]
-    user_ids.append user.id if user && !user.anonymous?
+    user_ids = [Discourse.system_user.id]
+    user_ids << user.id if user && !user.anonymous?
     group_ids = SiteSetting.private_topics_permitted_groups.split("|").map(&:to_i)
-    user_ids = user_ids + Group.where(id: group_ids).joins(:users).pluck('users.id')
+    user_ids += Group.where(id: group_ids).joins(:users).pluck('users.id')
     user_ids.uniq
   end
 
@@ -65,10 +69,10 @@ module ::DiscoursePrivateTopics
         each_with_object({}) do |record, h|
           h[record.category_id] = record.value.split(',').map(&:to_i)
         end
-      cat_group_map.merge! (excluded_map)
+      cat_group_map.merge!(excluded_map)
 
-      user_group_ids = user.groups.pluck(:id).to_a
-      cat_group_map = cat_group_map.reject { |k, v| (v & user_group_ids).any? }
+      user_group_ids = user.groups.pluck(:id)
+      cat_group_map.reject! { |_, group_ids| (group_ids & user_group_ids).any? }
     end
 
     cat_group_map.keys
@@ -84,12 +88,12 @@ after_initialize do
     def execute(readonly_mode: @readonly_mode)
       super
 
-      if SiteSetting.private_topics_enabled && !(SiteSetting.private_topics_admin_sees_all & @guardian&.user&.admin?)
+      if SiteSetting.private_topics_enabled && !(SiteSetting.private_topics_admin_sees_all && @guardian&.user&.admin?)
         cat_ids = DiscoursePrivateTopics.get_filtered_category_ids(@guardian.user)
         unless cat_ids.empty?
           user_ids = DiscoursePrivateTopics.get_unfiltered_user_ids(@guardian.user)
           @results.posts.delete_if do |post|
-            next false if user_ids.include? post&.user&.id
+            next false if user_ids.include?(post&.user&.id)
             post&.topic&.category&.id && cat_ids.include?(post.topic.category&.id)
           end
         end
@@ -107,7 +111,7 @@ after_initialize do
       allowed = org_can_see_topic?(topic, hide_deleted)
       return false unless allowed 
 
-      if SiteSetting.private_topics_enabled && !(SiteSetting.private_topics_admin_sees_all & @user&.admin?)
+      if SiteSetting.private_topics_enabled && !(SiteSetting.private_topics_admin_sees_all && @user&.admin?)
         return true unless topic&.category 
 
         user_ids = DiscoursePrivateTopics.get_unfiltered_user_ids(@user)
@@ -122,4 +126,7 @@ after_initialize do
       true
     end
   end
+
+  # Патчим методы поиска
+  Search.prepend(PrivateTopicsPatchSearch)
 end
